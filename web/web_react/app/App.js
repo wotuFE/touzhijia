26e7e4bb4b027cafa76105a4ce4a8b42b2ac50a8
hedongxiaoshimei <1219121810@qq.com>
2017-07-31 19:35:34 +0800

    删除废弃文件

--- web/web_react/app/App.js
+++ web/web_react/app/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom'
-import Routes from './Routes';
 import antd from 'antd';
 
 
 
 //入口文件
 ReactDOM.render((
-        <Routes />
+        <h1>wotu-react</h1>
      ),
      document.getElementById('app')
 );
